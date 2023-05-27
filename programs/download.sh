86864f6fd3fe6a3100e07ac557e0dfdd5e772008
Benjamin Belrhomari <b@belrhomari.fr>
2023-05-27 19:08:15 +0200

    install ytdl from github

--- programs/download.sh
+++ programs/download.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # python -m pip install --upgrade youtube_dl
+pip install --upgrade --force-reinstall "git+https://github.com/ytdl-org/youtube-dl.git"
 
 python -m youtube_dl -i -f bestaudio --extract-audio --audio-format vorbis --audio-quality 0 --yes-playlist -o "./output/%(title)s.%(ext)s" "PLVixfvPwDZUmf9eXxkJHjAsSgeDxIyTRx"
 

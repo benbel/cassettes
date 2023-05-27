32342dba90aab2a59b0559fd0d299b79678d83c3
Benjamin Belrhomari <b@belrhomari.fr>
2023-05-27 22:47:23 +0200

    Update download.sh

--- programs/download.sh
+++ programs/download.sh
@@ -3,7 +3,7 @@
 # python -m pip install --upgrade youtube_dl
 pip install --upgrade --force-reinstall "git+https://github.com/ytdl-org/youtube-dl.git"
 
-python -m youtube_dl -i -f bestaudio --extract-audio --audio-format vorbis --audio-quality 0 --yes-playlist -o "./output/%(title)s.%(ext)s" "PLVixfvPwDZUmf9eXxkJHjAsSgeDxIyTRx" --playlist-items 40
+python -m youtube_dl -i -f bestaudio --extract-audio --audio-format vorbis --audio-quality 0 --yes-playlist -o "./output/%(title)s.%(ext)s" "PLVixfvPwDZUmf9eXxkJHjAsSgeDxIyTRx"
 
 for file in ./output/*.ogg; do mv -i "$file" "${file/- Bibliothèque de l\'Aventure.ogg/.ogg}"; done
 
